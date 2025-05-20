0782a0a208fd5d06c49a8be0983a2ff5d228458b
Michael <michael.vardanyan@web.de>
2025-05-20 14:25:24 +0200

    - add WorkoutList component to Root Component - display component - change server port to 8081 for backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {WorkoutListComponent} from './component/workout/workout-list/workout-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [WorkoutListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
